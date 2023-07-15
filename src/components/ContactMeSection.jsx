d492ea4ee98c686d396da527d80bdad4d005e9b3
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-15 18:14:39 +0100

    Fix: project cards images link fix

--- src/components/ContactMeSection.jsx
+++ src/components/ContactMeSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -67,57 +67,37 @@ const LandingSection = () => {
             <VStack spacing={4}>
               <FormControl
                 isInvalid={
-                  formik.touched.firstName &&
-                  formik.errors.firstName
+                  formik.touched.firstName && formik.errors.firstName
                     ? true
                     : false
                 }
               >
-                <FormLabel htmlFor="firstName">
-                  Name
-                </FormLabel>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
                 <Input
                   id="firstName"
                   name="firstName"
                   {...formik.getFieldProps("firstName")}
                 />
-                <FormErrorMessage>
-                  {formik.errors.firstName}
-                </FormErrorMessage>
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
               </FormControl>
               <FormControl
                 isInvalid={
-                  formik.touched.email &&
-                  formik.errors.email
-                    ? true
-                    : false
+                  formik.touched.email && formik.errors.email ? true : false
                 }
               >
-                <FormLabel htmlFor="email">
-                  Email Address
-                </FormLabel>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
                 <Input
                   id="email"
                   name="email"
                   type="email"
                   {...formik.getFieldProps("email")}
                 />
-                <FormErrorMessage>
-                  {formik.errors.email}
-                </FormErrorMessage>
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
               </FormControl>
               <FormControl>
-                <FormLabel htmlFor="type">
-                  Type of enquiry
-                </FormLabel>
-                <Select
-                  id="type"
-                  name="type"
-                  {...formik.getFieldProps("type")}
-                >
-                  <option value="hireMe">
-                    Freelance project proposal
-                  </option>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select id="type" name="type" {...formik.getFieldProps("type")}>
+                  <option value="hireMe">Freelance project proposal</option>
                   <option value="openSource">
                     Open source consultancy session
                   </option>
@@ -126,30 +106,19 @@ const LandingSection = () => {
               </FormControl>
               <FormControl
                 isInvalid={
-                  formik.touched.comment &&
-                  formik.errors.comment
-                    ? true
-                    : false
+                  formik.touched.comment && formik.errors.comment ? true : false
                 }
               >
-                <FormLabel htmlFor="comment">
-                  Your message
-                </FormLabel>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
                 <Textarea
                   id="comment"
                   name="comment"
                   height={250}
                   {...formik.getFieldProps("comment")}
                 />
-                <FormErrorMessage>
-                  {formik.errors.comment}
-                </FormErrorMessage>
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button
-                type="submit"
-                colorScheme="purple"
-                width="full"
-              >
+              <Button type="submit" colorScheme="purple" width="full">
                 Submit
               </Button>
             </VStack>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -7,25 +7,25 @@ const projects = [
     title: "React Space",
     description:
       "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
-    imageLink: "/src/assets/images/photo1.jpg",
+    imageLink: "./src/assets/images/photo1.jpg",
   },
   {
     title: "React Infinite Scroll",
     description:
       "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
-    imageLink: "/src/assets/images/photo2.jpg",
+    imageLink: "./src/assets/images/photo2.jpg",
   },
   {
     title: "Photo Gallery",
     description:
       "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
-    imageLink: "/src/assets/images/photo3.jpg",
+    imageLink: "./src/assets/images/photo3.jpg",
   },
   {
     title: "Event planner",
     description:
       "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
-    imageLink: "/src/assets/images/photo4.jpg",
+    imageLink: "./src/assets/images/photo4.jpg",
   },
 ];
 
